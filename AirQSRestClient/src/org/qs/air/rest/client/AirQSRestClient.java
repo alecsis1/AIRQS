f8b6f0994d74337f416c17fffb53366efec2fed4
Alex <alexpopescu1@yahoo.com.au>
2018-04-28 01:41:37 +0300

    update

--- AirQSRestClient/src/org/qs/air/rest/client/AirQSRestClient.java
+++ AirQSRestClient/src/org/qs/air/rest/client/AirQSRestClient.java
@@ -1,8 +1,14 @@
 package org.qs.air.rest.client;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.client.InvocationCallback;
+import javax.ws.rs.core.Form;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -14,12 +20,80 @@ import org.qs.air.api.core.entities.AirMetric;
 
 public class AirQSRestClient {
 
-	// public static void main(String[] args) throws Exception {
-	//
-	// trySyncRestEasyClient();
-	// tryAsyncRestEasyClient();
-	//
-	// }
+	private static final ResteasyClient client = new ResteasyClientBuilder().disableTrustManager().build();
+
+	public static void main(String[] args) throws Exception {
+
+		String token = getToken();
+
+		// Add test entity to the cloud db
+		ResteasyWebTarget endpoint = client.target("https://airqs.symmetry-apps.org/rest" + "/api/v1/am");
+		Builder reqBuilder = endpoint.request().accept(MediaType.APPLICATION_JSON)
+				.header("Content-Type", MediaType.APPLICATION_JSON).header("Authorization", "Bearer " + token);
+
+		AirMetric am = new AirMetric();
+		am.setHum(1.1);
+		am.setMq135(2.2);
+		am.setMq2(3.3);
+		am.setMq3(4.4);
+		am.setMq7(5.5);
+		am.setPressure(6.6);
+		am.setTemp(7.7);
+		am.setTimestamp(System.currentTimeMillis());
+		am.setName("Test01AM");
+		Response response = reqBuilder.post(Entity.json(am));
+		AirMetric registeredAM = response.readEntity(AirMetric.class);
+
+		System.out.println(registeredAM.toString());
+
+		endpoint = client.target("https://airqs.symmetry-apps.org/rest" + "/api/v1/am/all");
+
+		reqBuilder = endpoint.request().accept(MediaType.APPLICATION_JSON)
+				.header("Content-Type", MediaType.APPLICATION_JSON).header("Authorization", "Bearer " + token);
+
+		response = reqBuilder.get();
+
+		AirMetric[] items = response.readEntity(AirMetric[].class);
+		System.out.println(items.length);
+	}
+
+	private static String getToken() {
+		Properties prop = new Properties();
+		InputStream in = null;
+		String user = null, pass = null;
+		try {
+
+			in = new FileInputStream("config.properties");
+			prop.load(in);
+
+			user = prop.getProperty("user", "");
+			pass = prop.getProperty("password", "");
+
+		} catch (IOException io) {
+			io.printStackTrace();
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		ResteasyWebTarget endpoint = client
+				.target("https://airqs.symmetry-apps.org/auth/realms/Google-Auth/protocol/openid-connect/token");
+
+		Builder reqBuilder = endpoint.request().header("Content-Type", "application/x-www-form-urlencoded");
+		Form form = new Form();
+		form.param("grant_type", "password").param("username", user).param("password", pass).param("client_id",
+				"AirQS");
+		Entity<Form> entity = Entity.form(form);
+		Response response = reqBuilder.post(entity);
+
+		TokenModel output = response.readEntity(TokenModel.class);
+
+		return output.getAccess_token();
+	}
 
 	public static AirMetric CreateMetricSyncRestEasy(AirMetric am, String url, String user, String pass) {
 		ResteasyClient client = new ResteasyClientBuilder().disableTrustManager().build();
@@ -28,9 +102,9 @@ public class AirQSRestClient {
 		client.register(bauth);
 
 		ResteasyWebTarget endpoint = client.target(url + "/api/v1/am");
-
-		Builder reqBuilder = endpoint.request().accept(MediaType.APPLICATION_JSON).header("Content-Type",
-				MediaType.APPLICATION_JSON);
+		String token = getToken();
+		Builder reqBuilder = endpoint.request().accept(MediaType.APPLICATION_JSON)
+				.header("Content-Type", MediaType.APPLICATION_JSON).header("Authorization", "Bearer " + token);
 
 		Response response = reqBuilder.post(Entity.json(am));
 
---
+++ AirQSRestClient/src/org/qs/air/rest/client/TokenModel.java
@@ -0,0 +1,80 @@
+package org.qs.air.rest.client;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class TokenModel implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String access_token;
+	private String refresh_token;
+	private String token_type;
+	private String session_state;
+	private int expires_in;
+	private int refresh_expires_in;
+
+	@JsonProperty("not-before-policy")
+	private int not_before_policy;
+
+	public TokenModel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getAccess_token() {
+		return access_token;
+	}
+
+	public void setAccess_token(String access_token) {
+		this.access_token = access_token;
+	}
+
+	public String getRefresh_token() {
+		return refresh_token;
+	}
+
+	public void setRefresh_token(String refresh_token) {
+		this.refresh_token = refresh_token;
+	}
+
+	public String getToken_type() {
+		return token_type;
+	}
+
+	public void setToken_type(String token_type) {
+		this.token_type = token_type;
+	}
+
+	public String getSession_state() {
+		return session_state;
+	}
+
+	public void setSession_state(String session_state) {
+		this.session_state = session_state;
+	}
+
+	public int getExpires_in() {
+		return expires_in;
+	}
+
+	public void setExpires_in(int expires_in) {
+		this.expires_in = expires_in;
+	}
+
+	public int getRefresh_expires_in() {
+		return refresh_expires_in;
+	}
+
+	public void setRefresh_expires_in(int refresh_expires_in) {
+		this.refresh_expires_in = refresh_expires_in;
+	}
+
+	public int getNot_before_policy() {
+		return not_before_policy;
+	}
+
+	public void setNot_before_policy(int not_before_policy) {
+		this.not_before_policy = not_before_policy;
+	}
+
+}
