29656e9da7ca65e7d6e75db80c06099640306475
Alex <alexpopescu1@yahoo.com.au>
2018-05-19 21:21:29 +0300

    entry base

--- AirQSModels/src/org/qs/air/api/base/EntityBase.java
+++ AirQSModels/src/org/qs/air/api/base/EntityBase.java
@@ -2,11 +2,16 @@ package org.qs.air.api.base;
 
 import java.io.Serializable;
 
+import javax.persistence.Basic;
 import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @XmlRootElement(name = "EntityBase")
 @XmlType(name = "EntityBase")
@@ -15,11 +20,9 @@ public class EntityBase implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	// @Basic(optional = false)
-	// @GeneratedValue(strategy = GenerationType.IDENTITY, generator =
-	// "DBIDGenerator")
-	// @GenericGenerator(name = "DBIDGenerator", strategy =
-	// "com.qfp.api.DBIDGenerator")
+	@Basic(optional = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY, generator = "DBIDGenerator")
+	@GenericGenerator(name = "DBIDGenerator", strategy = "org.qs.air.api.DBIDGenerator")
 	@Column(name = "id", nullable = false, unique = true, columnDefinition = " bigint AUTO_INCREMENT")
 	private long id;
 
